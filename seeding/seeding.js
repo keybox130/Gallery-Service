5fe034b4988148c4629d92be58b354253fee3b19
Emmanuel Mota <emmanuelmota@gmail.com>
2020-09-24 21:55:22 -0700

    Adds more seeding data and function Skeltons

--- seeding/seeding.js
+++ seeding/seeding.js
@@ -5,11 +5,27 @@ const imageSeeder= () => {
   const url = 'www.aws.com/imageurl.jpg';
   return url;
 };
+
 // Image description
 const imageDesc = ['Living Room', 'Dining Room', 'Kitchen', 'Bedroom', 'Breakfast Nook', 'Bathroom', 'Welcome Home', 'Comfortable Queen Bed', 'Flat screen cable TV and Comcast High Speed Internet.', 'Small Kitchen', 'Coffee Maker and Coffee', 'Bathroom with Shower', 'Off grid cob house on forty acres. Close to shower, outhouse and picnic table', 'The Cob House is in a very private setting all your own. Bring a camp stove and an ice chest and make your meals on the picnic table there.', 'The down comforter and extra blankets, 100% cotton sheets on this double bed. Warmed by the cob houses insulation and a propane heater if needed .', 'Our greenhouse covered outdoor shower in old redwood stumps. Surrounded on three sides by a bathhouse.', 'The outdoor shower', 'Our cute dog Scout will greet you. Dogs are okay with an extra $20 a night fee.', 'Cozy and tucked in.', 'The front porch', 'Propane fireplace and small kitchenette', 'Cozy queen-sized bed', 'Convertible sofa', 'Real bed! Built in', '"Smart" TV and WiFI', 'Living Area', 'Sleeper couch, fireplace, & TV', 'Sleeper couch and futon single bed', 'Kitchen and dining area', 'Front door (left) and pantry (right)', 'Work desk - if you have to work during your stay', ''];
+console.log(imageDesc.length);
 
 // Stay Name
-const stayName = ['Charming Craftsman East Sac/Midtown Apartment', 'Cozy Napa Wine Country Studio', 'The Cob house', 'Marina District Private Rooftop Studio with Deck', 'Serenity Seaside Garden Cottage (no cleaning fee)', 'Detached Wine Country "tiny house"', 'Sunset Yurt – stunning infinity pool', 'Downtown Cottage - Walk to Plaza!', 'Redwoods Treehouse', 'Bird Glamping', 'El Avistador. Montes de Toledo', 'Shepherd,s Hut with hot tub use near Stonehenge', ''];
-// Super Host
+const stayName = ['Charming Craftsman East Sac/Midtown Apartment', 'Cozy Napa Wine Country Studio', 'The Cob house', 'Marina District Private Rooftop Studio with Deck', 'Serenity Seaside Garden Cottage (no cleaning fee)', 'Detached Wine Country "tiny house"', 'Sunset Yurt – stunning infinity pool', 'Downtown Cottage - Walk to Plaza!', 'Redwoods Treehouse', 'Bird Glamping', 'El Avistador. Montes de Toledo', 'Shepherd,s Hut with hot tub use near Stonehenge'];
+console.log(stayName.length);
 
-const stayLocation = ['Sacramento', 'Angwin', 'Gualala', 'San Francisco', 'Santa Rosa', 'Garden Valley', 'Healdsburg', 'Sea Ranch', 'Sesquilé', 'Sonseca', ''];
+// Super Host
+const stayLocation = ['Sacramento', 'Angwin', 'Gualala', 'San Francisco', 'Santa Rosa', 'Garden Valley', 'Healdsburg', 'Sea Ranch', 'Sesquilé', 'Sonseca', 'DeLand Southwest', 'McKean', 'Fisher Island', 'Friendswood', 'Pewee Valley', 'Falling Waters', 'Anchor Bay', 'Poplar', 'Corona de Tucson', 'Longtown', 'Fidelity', 'Tropic', 'Pueblito del Carmen', 'Halstead', 'Tully', 'Miami Lakes', 'Maynard', 'Ocean City', 'Dillwyn', 'Lorenz Park', 'Fairacres', 'Hato Candal', 'King William', 'Springs', 'Bull Shoals', 'Upper Bear Creek', 'Watervliet', 'Grey Eagle', 'Kingston', 'Royal Lakes', 'Makakilo', 'Coffeen', 'Spangle', 'Alpine', 'Mango', 'Watsontown', 'Salmon', 'Ravalli', 'Lakewood Shores', 'Barahona', 'Locustdale', 'Dunnell', 'Chester Gap', 'North Fair Oaks', 'La Salle', 'Park Layne', 'Beechmont', 'Cornelia', 'Graceville', 'Trout Valley'];
+console.log(stayLocation.length);
+
+const rating = () => {
+
+};
+
+const seedTheDB = () => {
+// eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 100; i++) {
+
+
+ }
+};
